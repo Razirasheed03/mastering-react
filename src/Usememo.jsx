82b22e1e286a09053493b22bd887cf39c7e4ccb9
Razirasheed03 <razirasheed03@gmail.com>
2025-06-07 06:03:41 +0530

    Use memo for memoization

--- src/Usememo.jsx
+++ src/Usememo.jsx
@@ -1,26 +1,26 @@
 import React, { useMemo, useState } from 'react'
 
 const UseMemo = () => {
-    const [count,setCount]=useState(0)
-    const [namee,setName]=useState('First')
-    const expensiveCalc=useMemo(()=>{
-        let sum=0;
-        for(let i=0;i<100;i++){
-            sum+=i
+    const [count, setCount] = useState(0)
+    const [namee, setName] = useState('First')
+    const expensiveCalc = useMemo(() => {
+        let sum = 0;
+        for (let i = 0; i < 100; i++) {
+            sum += i
         }
-    },[count])
-  return (
-    <div>
-        {count}
-        <button onClick={()=>setCount(count+1)}>+</button>
-        {namee}
-        <button onClick={()=>setName('Second')}>NameChange</button>
-       
-       <p>
-       {expensiveCalc}
-        </p> 
-    </div>
-  )
+    }, [count])
+    return (
+        <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+            {namee}
+            <button onClick={() => setName('Second')}>NameChange</button>
+
+            <p>
+                {expensiveCalc}
+            </p>
+        </div>
+    )
 }
 
 export default UseMemo
