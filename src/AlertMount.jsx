abf8a79a54186a74379ff61e18b1176fd965469f
Razirasheed03 <razirasheed03@gmail.com>
2025-06-19 01:22:47 +0530

    small changes made

--- src/AlertMount.jsx
+++ src/AlertMount.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react'
 
-const Childs=()=>{
-    useEffect(()=>{
-        return ()=>{
+const Childs = () => {
+    useEffect(() => {
+        return () => {
             alert('ill unmount')
         }
-    },[])
+    }, [])
     return <h2>Im the child</h2>
 }
 
 const AlertMount = () => {
-    const [display,setDisplay]=useState(true)
-  return (
-    <div>
-        {display?<Childs/>:null}
-        <button onClick={()=>setDisplay(false)}>CLICK</button>
-    </div>
-  )
+    const [display, setDisplay] = useState(true)
+    return (
+        <div>
+            {display ? <Childs /> : null}
+            <button onClick={() => setDisplay(false)}>CLICK</button>
+        </div>
+    )
 }
 
 
